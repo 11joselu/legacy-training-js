113af4493b578d6bcc9180a29daa8f1dd482aa02
Jose Cabrera <jose@codium.team>
2022-01-04 15:16:15 +0100

    Wrap user info creation into a function

--- web-page-generator-kata/src/users_static_webpage_generator.js
+++ web-page-generator-kata/src/users_static_webpage_generator.js
@@ -1,29 +1,29 @@
-let fs = require("fs");
-var os = require("os");
+let fs = require('fs');
+var os = require('os');
 
 class UsersStaticWebpageGenerator {
   generateFile(users) {
-    var stream = fs.createWriteStream("html/users.html", { flags: "w" });
+    var stream = fs.createWriteStream('html/users.html', { flags: 'w' });
     var writeln = createWriteln(stream);
 
-    writeln("<!doctype html>");
+    writeln('<!doctype html>');
     writeln('<html lang="en">');
-    writeln("<head>");
+    writeln('<head>');
     writeln('<meta charset="utf-8">');
     writeln(
       '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
     );
 
-    writeln("<title>User biographies</title>");
+    writeln('<title>User biographies</title>');
 
-    writeln("<!-- Bootstrap core CSS -->");
+    writeln('<!-- Bootstrap core CSS -->');
     writeln(
       '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">'
     );
 
-    writeln("<!-- Custom styles for this template -->");
+    writeln('<!-- Custom styles for this template -->');
     writeln('<link href="assets/cover.css" rel="stylesheet">');
-    writeln("</head>");
+    writeln('</head>');
 
     writeln('<body class="text-center">');
 
@@ -37,30 +37,29 @@ class UsersStaticWebpageGenerator {
     writeln('<a class="nav-link active" href="#">Home</a>');
     writeln('<a class="nav-link" href="#">Features</a>');
     writeln('<a class="nav-link" href="#">Contact</a>');
-    writeln("</nav>");
-    writeln("</div>");
-    writeln("</header>");
+    writeln('</nav>');
+    writeln('</div>');
+    writeln('</header>');
 
     writeln('<main role="main" class="inner cover">');
     users.forEach((user) => {
-      writeln(`<h1 class=\"cover-heading\">${user.getName()}</h1>`);
-      writeln(`<p class=\"lead\">${user.getBiography()}</p>`);
+      createUserInfoBlock(writeln, user);
     });
-    writeln("</main>");
+    writeln('</main>');
 
     writeln('<footer class="mastfoot mt-auto">');
     writeln('<div class="inner">');
     writeln(
       '<p>Sprout class kata created by Codium <a href="https://twitter.com/CodiumTeam">@CodiumTeam</a>. Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>'
     );
-    writeln("</div>");
-    writeln("</footer>");
-    writeln("</div>");
+    writeln('</div>');
+    writeln('</footer>');
+    writeln('</div>');
 
-    writeln("<!-- Bootstrap core JavaScript");
-    writeln("        ================================================== -->");
+    writeln('<!-- Bootstrap core JavaScript');
+    writeln('        ================================================== -->');
     writeln(
-      "<!-- Placed at the end of the document so the pages load faster -->"
+      '<!-- Placed at the end of the document so the pages load faster -->'
     );
     writeln(
       '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>'
@@ -71,11 +70,16 @@ class UsersStaticWebpageGenerator {
     writeln(
       '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>'
     );
-    writeln("</body>");
-    writeln("</html>");
+    writeln('</body>');
+    writeln('</html>');
 
     stream.end();
   }
+}
+
+function createUserInfoBlock(writeln, user) {
+  writeln(`<h1 class=\"cover-heading\">${user.getName()}</h1>`);
+  writeln(`<p class=\"lead\">${user.getBiography()}</p>`);
 }
 
 function createWriteln(stream) {
