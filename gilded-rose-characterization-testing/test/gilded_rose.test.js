e320dbb41bd9268d40462b0adccd2fddafebf813
Jose Cabrera <jose@codium.team>
2022-01-11 16:39:12 +0100

    item with passed date should degreades quality twicas a fast

--- gilded-rose-characterization-testing/test/gilded_rose.test.js
+++ gilded-rose-characterization-testing/test/gilded_rose.test.js
@@ -11,5 +11,14 @@ describe('Gilded Rose', function () {
       expect(item.sellIn).toBe(9);
       expect(item.quality).toBe(19);
     });
+
+    it('And SellIn date has passed. Quality should degrades twice as fast', function () {
+      const gildedRose = new GildedRose([new Item('Book', 0, 20)]);
+
+      const [item] = gildedRose.updateQuality();
+
+      expect(item.name).toBe('Book');
+      expect(item.quality).toBe(18);
+    });
   });
 });
