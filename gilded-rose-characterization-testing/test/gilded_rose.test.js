2a96f677a2c1f5b2ead11114623048ad348dd5c8
Jose Cabrera <jose@codium.team>
2022-01-11 16:38:13 +0100

    Create basic test for non special item

--- gilded-rose-characterization-testing/test/gilded_rose.test.js
+++ gilded-rose-characterization-testing/test/gilded_rose.test.js
@@ -1,11 +1,15 @@
-const { GildedRose, Item } = require("../src/gilded_rose");
+const { GildedRose, Item } = require('../src/gilded_rose');
 
-describe("Gilded Rose", function () {
-  it("change me", function () {
-    const gildedRose = new GildedRose([new Item("Book", 10, 20)]);
+describe('Gilded Rose', function () {
+  describe('When a Item is not a special item', () => {
+    it('SellIn and Quality should be reduced by one', function () {
+      const gildedRose = new GildedRose([new Item('Book', 10, 20)]);
 
-    const items = gildedRose.updateQuality();
+      const [item] = gildedRose.updateQuality();
 
-    expect(items[0].name).toBe("Book");
+      expect(item.name).toBe('Book');
+      expect(item.sellIn).toBe(9);
+      expect(item.quality).toBe(19);
+    });
   });
 });
