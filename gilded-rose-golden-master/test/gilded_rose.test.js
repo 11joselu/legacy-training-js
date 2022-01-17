2d311037745708dc76d5c771bbdd72acc4904052
Jose Cabrera <jose@codium.team>
2022-01-17 17:10:28 +0100

    Apply Golden master for success/wrong result comparison

--- gilded-rose-golden-master/test/gilded_rose.test.js
+++ gilded-rose-golden-master/test/gilded_rose.test.js
@@ -1,11 +1,37 @@
-const { GildedRose, Item } = require("../src/gilded_rose");
+const { GildedRose, Item } = require('../src/gilded_rose');
+const expectedResult = require('./expectedResult.json');
+const wrongExpectedResult = require('./wrongExpectedResult.json');
 
-describe("Gilded Rose", function () {
-  it("change me", function () {
-    const gildedRose = new GildedRose([new Item("Book", 10, 20)]);
+describe('Gilded Rose', function () {
+  it('Should update quality and sellin of item list', function () {
+    const items = [
+      createItem('foo', -1, 0),
+      createItem('Aged Brie', 0, 2),
+      createItem('Backstage passes to a TAFKAL80ETC concert', 2, 49),
+      createItem('Sulfuras, Hand of Ragnaros', 6, 11),
+    ];
+    const gildedRose = new GildedRose(items);
 
-    const items = gildedRose.updateQuality();
+    const updatedQualityItems = gildedRose.updateQuality();
 
-    expect(items[0].name).toBe("Book");
+    expect(updatedQualityItems).toEqual(expectedResult);
+  });
+
+  it('Should update quality and sellin of item list but should not match it', function () {
+    const items = [
+      createItem('foo', -1, 0),
+      createItem('Aged Brie', 0, 2),
+      createItem('Backstage passes to a TAFKAL80ETC concert', 2, 49),
+      createItem('Sulfuras, Hand of Ragnaros', 6, 11),
+    ];
+    const gildedRose = new GildedRose(items);
+
+    const updatedQualityItems = gildedRose.updateQuality();
+
+    expect(updatedQualityItems).not.toEqual(wrongExpectedResult);
   });
 });
+
+function createItem(name, sellIn, quality) {
+  return new Item(name, sellIn, quality);
+}
